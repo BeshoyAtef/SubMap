c281df0bc5544ece4899efa62e508b63d1bbe869
BeshoSmile <beshosmile@gmail.com>
2013-11-25 18:13:08 +0200

    Fix submit method

--- userui/views.py
+++ userui/views.py
@@ -8,10 +8,13 @@ from django.template import RequestContext
 
 
 # Create your views here.
-def test(request):
-    return render(request,'testTrial.html')
-
 def test2(request):
+    available_trails = Trial.objects.all().order_by('order')
+    return render(request,'testTrial.html',{'trial': available_trails[0]})
+
+def test(request):
+    available_trails = Trial.objects.all().order_by('order')
+    return render(request,'testTrial.html',{'trial': available_trails[0]})
     return render(request,'testTrial2.html')
 
 def index(request):
@@ -19,7 +22,18 @@ def index(request):
     return render(request,'index.html',{'trials': available_trails})
 
 
-def submitter(request,block,trial,user):
+def renderer(request,user,block,trial):
+    user = UserProfile.objects.get(pk=user)
+    
+    answered_trials = (Results.objects.filter(uID=user,blockID=1).values_list('trialID', flat=True))
+    if answered_trials :
+        available_trails = Trial.objects.filter(blockId=1).exclude(trialNumber=answered_trials).order_by('order')
+    else:
+        available_trails = Trial.objects.filter(blockId=1).order_by('order')
+    return render(request,'index.html',{'trials': available_trails})
+
+def submitter(request,user,block,trial):
+    flag=saver(request,user,block,trial)
     user = UserProfile.objects.get(pk=user)
     
     answered_trials = (Results.objects.filter(uID=user,blockID=1).values_list('trialID', flat=True))
@@ -31,9 +45,11 @@ def submitter(request,block,trial,user):
 
 
 
-def saver(request,trail):
+
+def saver(request,user,block,trial):
+    user = UserProfile.objects.get(pk=user)
     answer = request.POST['answer']
-    result = Result(user=user,userblock=userblock,trial=trial,answer=answer)
+    result = Results(uID=user,blockID=block,trialID=trial,userAnswer=answer)
     result.save()
-    return result
+    return True
 
